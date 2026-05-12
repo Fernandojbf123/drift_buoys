bc07aa5c3b2fe44502ef5d8aab282d2b2d9cad2f
bellodev <fernandojbf123@gmail.com>
2026-05-12 12:51:33 -0600

    Add: manager de variables de datos de excel de las doris. Fix: configuracion de documentos y su manager, la funcion leer_excel_maestro se renombro a leer_excel y ahora lee cualquier excel

--- crear_documentos/configs/manager_doc_config.py
+++ crear_documentos/configs/manager_doc_config.py
@@ -6,23 +6,23 @@ importlib.reload(configs.configuracion_documentos)
 
 dotenv.load_dotenv()  # Carga las variables de entorno desde el archivo .env
 
-def get_usar_NAS():
+def get_orden_de_servicio():
     """Obtiene un valor de la configuración general de forma dinámica"""
-    return configs.configuracion_documentos.usar_NAS
+    return configs.configuracion_documentos.orden_de_servicio
 
-def get_ruta_al_excel_maestro():
+def ruta_al_excel_para_crear_documentos_101():
     """Obtiene un valor de la configuración general de forma dinámica"""
     ruta_al_NAS = os.getenv("ruta_al_NAS")
-    carpeta = configs.configuracion_documentos.ruta_al_excel_de_despliegue_de_sondas
+    carpeta = configs.configuracion_documentos.ruta_al_excel_para_crear_documentos_101
     if get_usar_NAS() and ruta_al_NAS:
         ruta_completa = os.path.join(ruta_al_NAS, carpeta)   
     else:
         ruta_completa = carpeta  # Si no se encuentra la variable de entorno, usar la ruta relativa     
     return ruta_completa
 
-def get_hoja_del_excel():
+def hoja_del_excel_para_crear_documetos_101():
     """Obtiene un valor de la configuración general de forma dinámica"""
-    return configs.configuracion_documentos.hoja_del_excel
+    return configs.configuracion_documentos.hoja_del_excel_para_crear_documetos_101
 
 def get_ruta_a_carpeta_de_las_figuras():
     """Obtiene un valor de la configuración general de forma dinámica"""
@@ -34,7 +34,6 @@ def get_ruta_a_carpeta_de_las_figuras():
         ruta_completa = carpeta  # Si no se encuentra la variable de entorno, usar la ruta relativa     
     return ruta_completa
 
-
 def get_ruta_a_carpeta_de_guardado_del_documento():
     """Obtiene un valor de la configuración general de forma dinámica"""
     ruta_al_NAS = os.getenv("ruta_al_NAS")
@@ -45,6 +44,26 @@ def get_ruta_a_carpeta_de_guardado_del_documento():
         ruta_completa = carpeta  # Si no se encuentra la variable de entorno, usar la ruta relativa     
     return ruta_completa
 
+def get_ruta_al_excel_de_despliegue_de_sondas():
+    """Obtiene un valor de la configuración general de forma dinámica"""
+    ruta_al_NAS = os.getenv("ruta_al_NAS")
+    carpeta = configs.configuracion_documentos.ruta_al_excel_de_despliegue_de_sondas
+    if get_usar_NAS() and ruta_al_NAS:
+        ruta_completa = os.path.join(ruta_al_NAS, carpeta)   
+    else:
+        ruta_completa = carpeta  # Si no se encuentra la variable de entorno, usar la ruta relativa     
+    return ruta_completa
 
-def get_nombre_de_la_plantilla_de_word():
-    return configs.configuracion_documentos.nombre_de_la_plantilla_de_word
+def get_hoja_del_excel_de_despliegue_de_sondas():
+    """Obtiene un valor de la configuración general de forma dinámica"""
+    return configs.configuracion_documentos.hoja_del_excel_de_despliegue_de_sondas
+
+def get_usar_NAS():
+    """Obtiene un valor de la configuración general de forma dinámica"""
+    return configs.configuracion_documentos.usar_NAS
+
+def nombre_de_la_plantilla_de_word_101():
+    return configs.configuracion_documentos.nombre_de_la_plantilla_de_word_101
+
+def nombre_de_la_plantilla_de_word_103():
+    return configs.configuracion_documentos.nombre_de_la_plantilla_de_word_103
--- crear_documentos/services/leer_excel.py
+++ crear_documentos/services/leer_excel.py
@@ -1,7 +1,7 @@
 import pandas as pd
-from crear_documentos.configs.manager_doc_config import *
+from configs.manager_doc_config import *
 
-def leer_excel_maestro(nombre_de_hoja: str = "instalacion", header=None) -> pd.DataFrame:
+def leer_excel(ruta_al_archivo, nombre_de_hoja: str = "instalacion", header=None) -> pd.DataFrame:
     """Lee el excel maestro con la información de los despliegues de sondas y devuelve un DataFrame de pandas.
 
     Descripción:
@@ -11,13 +11,13 @@ def leer_excel_maestro(nombre_de_hoja: str = "instalacion", header=None) -> pd.D
         y gráficos.
 
     Parámetros:
+        ruta_al_archivo (str): La ruta al archivo excel maestro a leer.
         nombre_de_hoja (str): El nombre de la hoja del excel maestro a leer. Por defecto es "instalacion".
         header (int, None): La fila o filas a usar como encabezado. Por defecto es None, lo que significa que no se usará ningún encabezado.
     Retorna:
         pandas.DataFrame: Un DataFrame con los datos del excel maestro, listo para ser filtrado y analizado."""
         
-    ruta = get_ruta_al_excel_maestro()+".xlsx"
-    with open(ruta, 'rb') as file:
+    with open(ruta_al_archivo, 'rb') as file:
         df_excel = pd.read_excel(file, sheet_name=nombre_de_hoja, header=header) 
     
     return df_excel
