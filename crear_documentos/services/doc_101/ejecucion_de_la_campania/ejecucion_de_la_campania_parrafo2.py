c815ddfeacad7e38579999bb014c23ac0a749a84
bellodev <fernandojbf123@gmail.com>
2026-05-13 10:35:05 -0600

    Fix: explicaciones para Metz

--- crear_documentos/services/doc_101/ejecucion_de_la_campania/ejecucion_de_la_campania_parrafo2.py
+++ crear_documentos/services/doc_101/ejecucion_de_la_campania/ejecucion_de_la_campania_parrafo2.py
@@ -7,19 +7,25 @@ def ejecucion_de_la_campania_parrafo2(dic_datos_doris: dict) -> str:
     
     df_unicos= get_fecha_y_hora_de_embarque_y_campania_unicos(df_datos_de_campanias)
     
-    
+    numero_de_sondas = get_numero_de_sondas(df_datos_doris)
     
     texto = "Dado que las actividades asociadas a la ejecución de "
     
     
     if len(df_unicos) == 1:
-        return "No se encontraron datos de campañas en el archivo Excel."
+        asm = df_unicos["campania"].iloc[0]
+        fecha = df_unicos["fecha_hora_de_embarque"].iloc[0]
+        fecha_campania = fecha.strftime("%d de %B de %Y") 
+        
+        
+        texto = "La ejecución de la campaña de instalación de las sondas, se efectuó de acuerdo con la planeación de la logística y derrotero de la embarcación "
+        
+        texto += "la campaña " + asm + " realizada el " + fecha_campania,  
+        return convertir_texto(texto, numero_de_sondas, numero_de_campanias)
     
     
     else:
-        for index, row in df_unicos.iterrows():
-            campania = row["campania"]
-            fecha_y_hora_de_campania = row["fecha_hora_de_embarque"]
+        pass
     
         
     
