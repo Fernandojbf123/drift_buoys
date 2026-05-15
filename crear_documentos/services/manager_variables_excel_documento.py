7c448104d8a6d9ec033f22f9c33d3c6e74be9e0a
bellodev <fernandojbf123@gmail.com>
2026-05-15 09:24:50 -0600

    add: Se creo el manager de documento

--- crear_documentos/services/manager_variables_excel_documento.py
+++ crear_documentos/services/manager_variables_excel_documento.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from configs.manager_doc_config import *
 from services.leer_excel import leer_excel
@@ -9,11 +10,12 @@ def get_df_datos_documento() -> pd.DataFrame:
     df_datos_documento = leer_excel(ruta_al_excel_para_crear_documento(), nombre_de_hoja=hoja_del_excel_para_crear_documento(), header=None)
     return df_datos_documento
 
-def get_variable_documento(df_datos_documento: pd.DataFrame, nombre_variable: str) -> str:
+def get_variable_documento(df_datos_documento: pd.DataFrame, nombre_variable: str) -> None | np.ndarray:
     varvalue = None
     for row in df_datos_documento.iterrows():
         if row[1][0] == nombre_variable:
-            varvalue = row[1][:]
+            varvalue = np.array(row[1][1:])
+            varvalue = varvalue[~pd.isna(varvalue)]  # Eliminar valores NaN
             return varvalue
     
     if varvalue is None:
