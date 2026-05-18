c27b810a893fb05922575a448c7289b6b1fef046
Metzli <metzoceanica@gmail.com>
2026-05-18 12:25:24 -0600

    Se construyo la logica de los parrafos para el capitulo ejecucion de la campania.

--- crear_documentos/services/doc_101/ejecucion_de_la_campania/ejecucion_de_la_campania_parrafo4.py
+++ crear_documentos/services/doc_101/ejecucion_de_la_campania/ejecucion_de_la_campania_parrafo4.py
@@ -1,11 +1,20 @@
+from configs.manager_doc_config import *
+from services.manager_variables_excel_datos_despliegue import *
+from services.manager_variables_excel_datos_campania import *
+
 
 def ejecucion_de_la_campania_parrafo4(dic_datos_doris: dict) -> str:
-#esta funcion tiene pendiente agregar las referencias cruzadas para el word 
-
-if 
-    texto = f"La Figura 8 y Figura 9 muestran"
+    dos_despliegues= get_fecha_y_hora_de_embarque_y_campania_unicos(dic_datos_doris)
     
-else:
-    texto = f"La Figura 8 muestra"
+    if len(dos_despliegues) == 1:
+        return (
+            f"La Figura {dos_despliegues[0]} "
+            "muestra las localizaciones solicitadas."
+        )
 
-    return texto
+    numeros = " y ".join(map(str, dos_despliegues))
+
+    return (
+        f"Las Figuras {numeros} "
+        "muestran las localizaciones solicitadas."
+    )
