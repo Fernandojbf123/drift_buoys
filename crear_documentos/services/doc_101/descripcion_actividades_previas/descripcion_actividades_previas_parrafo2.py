78f602fc14cdc9bea4f81f15a027d5a8ef266ac9
Metzli <metzoceanica@gmail.com>
2026-05-18 07:53:50 -0600

    En proceso de creacion de funciones para cada capitulo

--- crear_documentos/services/doc_101/descripcion_actividades_previas/descripcion_actividades_previas_parrafo2.py
+++ crear_documentos/services/doc_101/descripcion_actividades_previas/descripcion_actividades_previas_parrafo2.py
@@ -1,16 +1,15 @@
 from configs.manager_doc_config import *
 from services.manager_variables_excel_datos_despliegue import *
 from services.manager_variables_excel_datos_campania import *
-    
-    
+
 def descripcion_actividades_previas_parrafo2(dic_datos_doris: dict) -> str:
-    df_unicos= get_fecha_y_hora_de_embarque_y_campania_unicos(df_datos_de_campanias)
+    df_unicos = get_fecha_y_hora_de_embarque_y_campania_unicos(df_datos_de_campanias)
+    numero_de_sondas = get_numero_de_sondas(df_datos_doris)
     
-    if len(df_unicos) == 1:
-        numero_de_sondas = get_numero_de_sondas(df_datos_doris)
-        
-        texto = f"Las actividades previas a la campaña de liberación, comenzaron una vez que PEMEX proporcionó al personal de ASM-CICESE las coordenadas para el despliegue de las {numero_de_sondas} sondas oceanográficas, diseñadas conforme a las necesidades específicas de PEMEX. Previo al despliegue de las sondas se realizaron las actividades determinadas de acuerdo al plan de trabajo del personal y, en la medida de lo posible, se consideraron los tiempos programados en el Plan de Campaña de cada despliegue. "
-    else:
-        texto = f"Las actividades previas a las campañas de liberación, comenzaron una vez que PEMEX proporcionó al personal de ASM-CICESE las coordenadas para el despliegue de las {numero_de_sondas} sondas oceanográficas, diseñadas conforme a las necesidades específicas de PEMEX. Previo al despliegue de las sondas se realizaron las actividades determinadas de acuerdo al plan de trabajo del personal y, en la medida de lo posible, se consideraron los tiempos programados en el Plan de Campaña de cada despliegue. "
+    es_una_campania = len(df_unicos) == 1
+    articulo = "la" if es_una_campania else "las"
+    palabra_campania = "campaña" if es_una_campania else "campañas"
     
-    return texto 
+    texto = f"Las actividades previas a {articulo} {palabra_campania} de liberación, comenzaron una vez que PEMEX proporcionó al personal de ASM-CICESE las coordenadas para el despliegue de las {numero_de_sondas} sondas oceanográficas, diseñadas conforme a las necesidades específicas de PEMEX. Previo al despliegue de las sondas se realizaron las actividades determinadas de acuerdo al plan de trabajo del personal y, en la medida de lo posible, se consideraron los tiempos programados en el Plan de Campaña de cada despliegue. "
+    
+    return texto
