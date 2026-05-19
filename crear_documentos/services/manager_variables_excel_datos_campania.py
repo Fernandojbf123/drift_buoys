5f0692a73611457e1c40fdf284130e2fcb95cf68
Metzli <metzoceanica@gmail.com>
2026-05-19 12:55:34 -0600

    Merge branch 'desarrollo' into metzDrift

--- crear_documentos/services/manager_variables_excel_datos_campania.py
+++ crear_documentos/services/manager_variables_excel_datos_campania.py
@@ -56,3 +56,4 @@ def get_lon_plan(df_datos_campanias: pd.DataFrame) -> list[float]:
 def get_fecha_y_hora_de_embarque_y_campania_unicos(df_datos_campanias: pd.DataFrame) -> pd.DataFrame:
     df_output = df_datos_campanias[["fecha_hora_de_embarque", "campania"]].drop_duplicates()
     return df_output
+
