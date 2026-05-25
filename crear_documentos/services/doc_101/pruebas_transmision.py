b10fa4cfa17e1f21084bdc8ac98a191f7e1cf50e
Metzli <metzoceanica@gmail.com>
2026-05-25 10:24:51 -0600

    ok

--- crear_documentos/services/doc_101/pruebas_transmision.py
+++ crear_documentos/services/doc_101/pruebas_transmision.py
@@ -1,8 +1,8 @@
 from capturar_excel_a_png import capturar_excel_a_png
 
 ruta_png = capturar_excel_a_png(
-    ruta_excel="//Reportes_Edit/Reporte_10.1/202604/despliegue_18/pruebas_lab/prueba_en_tierra_9878221_TOTAL.xlsx",
-    carpeta_salida="//Reportes_Edit/Reporte_10.1/202604/documento/prueba_de_transmision_9878221.png"
+    ruta_excel="//Reporte_10.1/202604/despliegue_18/pruebas_lab/prueba_en_tierra_9878221_TOTAL.xlsx",
+    carpeta_salida="//Reporte_10.1/202604/documento/prueba_de_transmision_9878221.png"
 )
 print(ruta_png)
 
