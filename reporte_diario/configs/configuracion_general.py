4362c9ba6b26d3cec337d7f574d7dfaf17728208
bellodev <fernandojbf123@gmail.com>
2026-07-03 09:09:21 -0600

    Fix: Se hacen ajustes menores. Pendiente: Modificar archivos de config en todos los módulos para que sean varios objs que se pase a las funciones main de cada modulo

--- reporte_diario/configs/configuracion_general.py
+++ reporte_diario/configs/configuracion_general.py
@@ -1,5 +1,5 @@
 
 
 
-seriales_de_sondas = ["4876187","9878504", "9878225", "4910070","4912212","4904116","4912213","4857577","4912171","4912199"] # Despliegues de mayo 2026
+seriales_de_sondas = ["4876187","9878504", "4910070","4912212","4904116","4912213","4857577","4912171","4912199"] # Despliegues de mayo 2026
 carpeta_de_datos_crudos = "C:\\Users\\Atmosfera\\Desktop\\datos_crudos\\doris\\todos_los_datos" # Para pruebas de lab
--- reporte_diario/services/revisar_anomalias.py
+++ reporte_diario/services/revisar_anomalias.py
@@ -31,21 +31,21 @@ def revisar_anomalias():
             df["fecha"] = pd.to_datetime(df["fecha"], format="%Y-%m-%dT%H:%M:%S.%fZ", errors='coerce')  # Convertir la columna 'fecha' a datetime
             # Los datos vienen en hora UTC; cambiarlos a CST
             df["fecha"] = df["fecha"]-pd.Timedelta(hours=6)  # Restar 6 horas para convertir de UTC a CST
-            
-            today = pd.Timestamp.now().strftime("%Y-%m-%d %H:00")
-            yesterday = (pd.Timestamp.now() - pd.Timedelta(days=1))
-            df_cortado = df[df["fecha"] >= pd.Timestamp(yesterday)]  # Filtrar por fecha
-            
-        
-            if df_cortado.empty:
-                print(f"El archivo para el serial {serial} está vacío.")
-                max_fecha = df["fecha"].max().strftime("%d/%m/%y %H:%M")
-                output_dict["tiene_valores_anomalos_en_las_ultimas_24_horas"].append(f"No. La sonda fue desactivada el {max_fecha}")
-            
-            else:
-                
-                velocidad_mayor_a_2 = df_cortado[df_cortado["speed"] > 2]
+            fecha_de_ultima_medicion = df["fecha"].max().strftime("%d/%m/%y %H:%M")
 
+            # Crear un DF filtrado para los últimos 24 horas
+            yesterday_tspan = (pd.Timestamp.now() - pd.Timedelta(days=1))
+            df_cortado = df[df["fecha"] >= pd.Timestamp(yesterday_tspan)]  # Filtrar por fecha
+
+            # Si el df_cortado está vacío (No hay datos en las últimas 24 horas)
+            if df_cortado.empty:
+                print(f"El archivo para el serial {serial} no tiene datos en las últimas 24 horas.")
+                output_dict["tiene_valores_anomalos_en_las_ultimas_24_horas"].append(f"No. La sonda fue desactivada el {fecha_de_ultima_medicion}")
+            
+            # Si el df_cortado tiene datos (hay datos en las últimas 24 horas)
+            else: 
+                # Busco si tiene valores anómalos de velocidad
+                velocidad_mayor_a_2 = df_cortado[df_cortado["speed"] > 2]
                 if velocidad_mayor_a_2.empty:
                     output_dict["tiene_valores_anomalos_en_las_ultimas_24_horas"].append("No")
                 else:
@@ -55,8 +55,9 @@ def revisar_anomalias():
                     print(" ")
                     output_dict["tiene_valores_anomalos_en_las_ultimas_24_horas"].append(f"Si")
                     
+                    
             output_dict["serial"].append(serial)
-            output_dict["fecha_y_hora_de_ultima_medicion_CST"].append(df_cortado["fecha"].max().strftime("%d/%m/%y %H:%M"))
+            output_dict["fecha_y_hora_de_ultima_medicion_CST"].append(fecha_de_ultima_medicion)
                     
 
     df_salida = pd.DataFrame(output_dict)
