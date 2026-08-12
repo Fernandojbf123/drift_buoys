54033da90c685b1a906c33319c26359952516b81
bellodev <fernandojbf123@gmail.com>
2026-08-12 08:52:52 -0600

    Fix: Git ignore ya no rastrea los cambios en los archivos de configuracion

--- descargar_datos/configs/configuracion_general.py
+++ descargar_datos/configs/configuracion_general.py
@@ -1,5 +1,5 @@
 
 
 
-seriales_de_sondas = ["4910070","4904116","4912213","4912208", "4912223", "9909282", "4912209"] # Despliegues de mayo 2026
+seriales_de_sondas = ["4910070","4904116","4912213","4912208", "4912223", "4912209"] # Despliegues de mayo 2026
 carpeta_de_datos_crudos = "C:\\Users\\Atmosfera\\Desktop\\datos_crudos\\doris\\todos_los_datos" # Para pruebas de lab
